be0dd4202bc7352ec083da9b7b0e101b017cce0a
miller2590 <miller2590@gmail.com>
2021-11-20 17:10:43 -0600

    Added search bar functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
     this.state = {
       // Search Results State
       searchResults: [
@@ -78,6 +80,16 @@ class App extends React.Component {
     this.setState({ playlistName: name });
   }
 
+  // Saves playlist using Spotify API
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+  }
+
+  // Sets the search bar to a Spotify endpoint
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
@@ -85,7 +97,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
@@ -96,6 +108,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
